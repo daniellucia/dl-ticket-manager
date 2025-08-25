2f49ab3877a469af098f27655e317d79796375d0
Daniel Lúcia <dlucia@refineria.es>
2025-08-25 19:58:09 +0200

    Espacios

--- src/Plugin.php
+++ src/Plugin.php
@@ -54,19 +54,13 @@ class Plugin
     public function loadComponents(): void
     {
         $this->product_type->register();
-
         $this->form_handler->register();
-
         $this->ticket_generator->register();
-
         $this->cpt->registerCpt();
         $this->cpt->registerTaxonomy();
         $this->cpt->register();
-
         $this->pdf->maybeDownloadTicket();
-
         $this->config_page->register();
-
         $this->validation->register();
     }
 }
