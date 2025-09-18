8143e24ab911125149f275b2bdc3b67d4073a4c9
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-18 13:04:14 +0200

    Quitamos clases innecesarias

--- src/Views/Account.php
+++ src/Views/Account.php
@@ -2,30 +2,30 @@
 <table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table">
     <thead>
         <tr>
-            <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-order-number"><span class="nobr"><?= __('Event', 'dl-ticket-manager') ?></span></th>
-            <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-order-date"><span class="nobr"><?= __('Date', 'dl-ticket-manager') ?></span></th>
-            <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-order-status"><span class="nobr"><?= __('Name', 'dl-ticket-manager') ?></span></th>
-            <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-order-total"><span class="nobr"><?= __('Identifier', 'dl-ticket-manager') ?></span></th>
-            <th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-order-actions"><span class="nobr"><?= __('Download ticket', 'dl-ticket-manager') ?></span></th>
+            <th scope="col" class="woocommerce-orders-table__header"><span class="nobr"><?= __('Event', 'dl-ticket-manager') ?></span></th>
+            <th scope="col" class="woocommerce-orders-table__header"><span class="nobr"><?= __('Date', 'dl-ticket-manager') ?></span></th>
+            <th scope="col" class="woocommerce-orders-table__header"><span class="nobr"><?= __('Name', 'dl-ticket-manager') ?></span></th>
+            <th scope="col" class="woocommerce-orders-table__header"><span class="nobr"><?= __('Identifier', 'dl-ticket-manager') ?></span></th>
+            <th scope="col" class="woocommerce-orders-table__header"><span class="nobr"><?= __('Download ticket', 'dl-ticket-manager') ?></span></th>
         </tr>
     </thead>
 
     <tbody>
         <?php foreach ($tickets as $ticket): ?>
             <tr class="woocommerce-orders-table__row woocommerce-orders-table__row--status-processing order">
-                <th class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-number" scope="row">
+                <th class="woocommerce-orders-table__cell" scope="row">
                     <?= $this->e($ticket['event']) ?>
                 </th>
-                <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-date">
+                <td class="woocommerce-orders-table__cell">
                     <?= $this->e(date_i18n(get_option('date_format'), timestamp_with_offset: strtotime($ticket['date']))) ?>
                 </td>
-                <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-status" dta-title="Estado">
+                <td class="woocommerce-orders-table__cell" dta-title="Estado">
                     <?= $this->e($ticket['name']) ?>
                 </td>
-                <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-total">
+                <td class="woocommerce-orders-table__cell">
                     <?= $this->e($ticket['identifier']) ?>
                 </td>
-                <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-actions">
+                <td class="woocommerce-orders-table__cell">
                     <a href="<?= $pdf->url($ticket['code']); ?>" class="woocommerce-button wp-element-button button view"><?= __('Download ticket', 'dl-ticket-manager') ?></a>
                 </td>
             </tr>
