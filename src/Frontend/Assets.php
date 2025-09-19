4aeb463648a6b650e718f29f6c893694a31581f8
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-19 07:25:03 +0200

    Corrección para añadir el css en el hook correcto

--- src/Frontend/Assets.php
+++ src/Frontend/Assets.php
@@ -8,6 +8,16 @@ class Assets
 {
     public function __construct()
     {
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_style']);
+    }
+
+    /**
+     * Encola los estilos necesarios para el frontend
+     * @return void
+     * @author Daniel Lucia
+     */
+    public function enqueue_style()
+    {
         wp_enqueue_style(
             'tickets-css',
             plugin_dir_url(DL_TICKET_MANAGER_FILE) . 'assets/css/tickets.css',
