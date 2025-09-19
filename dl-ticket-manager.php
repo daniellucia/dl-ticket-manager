d6ad95af88ef85e6b7b0eb2546a0e289b353af83
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-19 08:27:07 +0200

    Fix

--- dl-ticket-manager.php
+++ dl-ticket-manager.php
@@ -38,9 +38,10 @@ define('DL_TICKET_MANAGER_VERSION', '1.0.0');
 define('DL_TICKET_MANAGER_FILE', __FILE__);
 
 add_action('init', function () {
-
     load_plugin_textdomain('dl-ticket-manager', false, dirname(plugin_basename(__FILE__)) . '/languages');
+});
 
+add_action('plugins_loaded', function () {
     $plugin = new Plugin();
     $plugin->init();
 });
